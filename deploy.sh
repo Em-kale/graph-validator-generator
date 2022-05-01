4777e75ab59e31acb84cdf72f69cb589b5923fac
Emmitt Luhning <49374241+Em-kale@users.noreply.github.com>
2022-05-01 19:00:50 -0400

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-for dir in ~/projects/graphs/auto-graphs/generated_graphs/*;
+for dir in ~<path-to>/generated_graphs/*;
   do 
      [ -d "$dir" ] && cd "$dir" && echo "Entering into $dir and installing packages" && npm install && graph auth --product hosted-service <access-key> && npm run codegen  && npm run deploy
   done;
